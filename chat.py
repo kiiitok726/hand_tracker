939d50f0bee6540deb33b7750adb2692c3e1672b
Kaito Kanazawa <kaito.kanazawa@gmail.com>
2025-04-21 21:30:39 +0900

    Cleaned code, added comments, reset camera frame, added color visualizations for each function, added a screenshot function

--- chat.py
+++ chat.py
@@ -2,53 +2,69 @@ import cv2
 import mediapipe as mp
 import pyautogui
 import math
-
-import whisper
-import sounddevice as sd
-import numpy as np
-import scipy.io.wavfile as wav
+import time
+import subprocess
 
 
 # booleans to set scrolling modes
 scroll_up = False
 scroll_down = False
+clicking = False
+ssing = False
 
 # simple math distance formula to detect finger proximity
 def distance(coords1, coords2):
     return math.sqrt((coords1[0]-coords2[0])**2 + (coords1[1]-coords2[1])**2)
  
 
-# initialize the media pipe hands
+# initialize the media pipe hands (up to 2 hands)
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(
     static_image_mode=False,
-    max_num_hands=2,
-    min_detection_confidence=0.7,
-    min_tracking_confidence=0.7
-)
+    max_num_hands=2, 
+    )
 mp_drawing = mp.solutions.drawing_utils
 
-# Start capturing video from the webcam.
-try:
-    cap = cv2.VideoCapture(0)
-except:
-    print("Wrong camera")
 
-cv2.namedWindow("Hand Tracking", cv2.WND_PROP_FULLSCREEN)
-cv2.setWindowProperty("Hand Tracking", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+# Start capturing video from the webcam, try cam 1 if cam 0 does not work
+# try:
+#     cap = cv2.VideoCapture(1)
+#     print("Camera 1 Activated")
+# except:
+#     cap = cv2.VideoCapture(0)
+#     print("Camera 0 Activated")
+
+for i in range(2):
+    cap = cv2.VideoCapture(i)
+    if cap.isOpened():
+        break
+
+# # Set camera window properties to full screen
+# cv2.namedWindow("Hand Tracking", cv2.WND_PROP_FULLSCREEN)
+# cv2.setWindowProperty("Hand Tracking", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+# Create window
+cv2.namedWindow("Hand Track Cam")
 
 
-
-
+# Main loop, ran every frame
 while cap.isOpened():
     ret, frame = cap.read()
+
     if not ret:
         continue
 
-    # Flip the frame horizontally for a mirror view.
+    # Flip window(the frame array) by horizontal axis only
     frame = cv2.flip(frame, 1)
-    # h, w, c = frame.shape
-    w, h = pyautogui.size()
+
+    # Find camera window dimensions
+    h, w, c = frame.shape
+
+    color = (255, 0, 0)
+
+    # print(frame.shape)
+    # print(pyautogui.size())
+    # w, h = pyautogui.size()
 
     # Convert the BGR image to RGB.
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -66,70 +82,101 @@ while cap.isOpened():
             index_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
             middle_tip = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP]
             ring_tip = hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP]
+            pinky_tip = hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_TIP]
 
-            # Convert normalized coordinates to pixel coordinates.
+            # Convert normalized coordinates(0-1) to screen's pixel coordinates
             thumb_coords = (int(thumb_tip.x * w), int(thumb_tip.y * h))
             index_coords = (int(index_tip.x * w), int(index_tip.y * h))
             middle_coords = (int(middle_tip.x * w), int(middle_tip.y * h))
             ring_coords = (int(ring_tip.x * w), int(ring_tip.y * h))
+            pinky_coords = (int(pinky_tip.x * w), int(pinky_tip.y * h))
 
             palm_x = sum([hand_landmarks.landmark[i].x for i in indices]) / len(indices)
             palm_y = sum([hand_landmarks.landmark[i].y for i in indices]) / len(indices)
             palm_coords = (int(palm_x * w), int(palm_y * h))
             
-            # Draw a circle at the computed palm center.
-            cv2.circle(frame, palm_coords, 10, (0, 255, 0), -1)
 
-            # Draw a circle (node) at each finger tip.
-            cv2.circle(frame, thumb_coords *w, 5, (0, 255, 0), -1)
-            cv2.circle(frame, index_coords *w, 5, (0, 255, 0), -1)
-            cv2.circle(frame, middle_coords *w, 5, (0, 255, 0), -1)
-            cv2.circle(frame, ring_coords *w, 5, (0, 255, 0), -1)
-
-            ## Draw lines connecting thumb to index and index to middle.
-            # cv2.line(frame, thumb_coords, index_coords, (255, 0, 0), 2)
-            # cv2.line(frame, index_coords, middle_coords, (255, 0, 0), 2)
-            # cv2.line(frame, middle_coords, ring_coords, (255, 0, 0), 2)
-            
-            # Optionally, draw all hand landmarks for context.
+            # Optionally, draw all hand landmarks 
             # mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
-            if distance(index_coords, middle_coords) < 35:
+
+            # DETECTIONS -------------------------------------------------------------------------
+
+            # Check for scroll
+            if distance(index_coords, middle_coords) < 40:
                 scroll_up = True
-            elif distance(middle_coords, ring_coords) < 35:
+            elif distance(middle_coords, ring_coords) < 40:
                 scroll_down = True
+            
+            # Check for click
+            if distance(middle_coords, thumb_coords) < 40:
+                clicking = True
+
+            # Check for ss
+            if distance(ring_coords, palm_coords) < 25:
+                ssing = True
                 
 
+            # EXECUTIONS --------------------------------------------------------------------------
 
+            # Move cursor to index finger
+            pyautogui.moveTo(index_coords[0], index_coords[1], _pause=False)
+
+            # Scroll
             if scroll_up:
                 pyautogui.scroll(-3, _pause=False)
                 color = (0, 0, 255)
             elif scroll_down:
                 pyautogui.scroll(3, _pause=False)
                 color = (0, 0, 255)
-            else:
-                color = (255, 0, 0)
-                # Move the mouse cursor to the palm center.
-                pyautogui.moveTo(index_coords[0], index_coords[1], _pause=False)
 
-                if distance(middle_coords, thumb_coords) < 35:
-                    pyautogui.click(_pause=False)
-                    print("clicking")
-                    print(distance(middle_coords, thumb_coords))
+            # Click
+            if clicking:
+                pyautogui.click(_pause=False)
+                color = (0, 255, 0)
 
-            # Draw lines connecting thumb to index and index to middle.
-            cv2.line(frame, thumb_coords, index_coords, (255, 0, 0), 2)
-            cv2.line(frame, index_coords, middle_coords, (255, 0, 0), 2)
-            cv2.line(frame, middle_coords, ring_coords, (255, 0, 0), 2)
+            # SS
+            if ssing:
+                pyautogui.screenshot(f"Hand Track Cam - {time.asctime()}")
+                subprocess.run(["screencapture", f"/Users/kaito/Desktop/Hand Track Cam - {time.asctime()}.png"])
+                color = (0, 255, 255)
+            
+
+            # DRAWINGS ----------------------------------------------------------------------------
+
+            # Draw node at palm
+            cv2.circle(frame, palm_coords, 15, (0, 255, 0), -1)
+
+            # Draw node at each fingertip
+            cv2.circle(frame, thumb_coords, 10, (0, 255, 0), -1)
+            cv2.circle(frame, index_coords, 10, (0, 255, 0), -1)
+            cv2.circle(frame, middle_coords, 10, (0, 255, 0), -1)
+            cv2.circle(frame, ring_coords, 10, (0, 255, 0), -1)
+            cv2.circle(frame, pinky_coords, 10, (0, 255, 0), -1)
+
+            # Draw connecting lines
+            cv2.line(frame, thumb_coords, index_coords, color, 2)
+            cv2.line(frame, index_coords, middle_coords, color, 2)
+            cv2.line(frame, middle_coords, ring_coords, color, 2)
+            cv2.line(frame, ring_coords, pinky_coords, color, 2)
         
 
+        # RESET ----------------------------------------------------------------------------
         scroll_up = False
         scroll_down = False
+        clicking = False
+        ssing = False
+
+        color = (255, 0, 0)
 
 
 
-    # Display the resulting frame.
-    cv2.imshow("Hand Tracking", frame)
+    # # Display the resulting frame.
+    # cv2.imshow("Hand Track Cam", frame)
+
+    # resized_frame = cv2.resize(frame, (2560, 1600))
+    # resized_frame = cv2.resize(frame, (1600, 1000))
+    cv2.imshow("Hand Track Cam", frame)
 
     # Press 'q' key to exit.
     if cv2.waitKey(1) & 0xFF == ord('q'):
